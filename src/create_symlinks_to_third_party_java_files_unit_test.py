7d602fc59455ebd2fcfc09836992691738662b6c
noel.yap <noel.yap@67170cb4-ae01-11dd-9253-9d056a18d4a0>
2009-01-26 23:37:13 +0000

    Initial commit.

---
+++ src/create_symlinks_to_third_party_java_files_unit_test.py
@@ -0,0 +1,526 @@
+#!/usr/bin/python2.4
+
+import mox
+import os
+import os.path
+import unittest
+
+import create_symlinks_to_third_party_java_files
+
+
+class CreateSymlinksTestCase(unittest.TestCase):
+  def testCreateSymlinkWhenFromFileDoesNotExist(self):
+    expected = False
+
+    from_file = 'from-file'
+    to_file = 'to-file'
+
+    mox_factory = mox.Mox()
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.exists(from_file).AndReturn(False)
+
+    mox_factory.ReplayAll()
+
+    observed = (
+        create_symlinks_to_third_party_java_files.CreateSymlink(
+            from_file, to_file, path=path_mock))
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+  def testCreateSymlinkWhenToFileExists(self):
+    expected = False
+
+    from_file = 'from-file'
+    to_file = 'to-file'
+
+    mox_factory = mox.Mox()
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.exists(from_file).AndReturn(True)
+    path_mock.exists(to_file).AndReturn(True)
+
+    mox_factory.ReplayAll()
+
+    observed = (
+        create_symlinks_to_third_party_java_files.CreateSymlink(
+            from_file, to_file, path=path_mock))
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+  def testCreateSymlinkWhenToDirExists(self):
+    expected = True
+
+    from_file = 'from/file'
+    to_file = 'to/file'
+    to_dir = 'to'
+
+    mox_factory = mox.Mox()
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.exists(from_file).AndReturn(True)
+    path_mock.exists(to_file).AndReturn(False)
+    path_mock.dirname(to_file).AndReturn(to_dir)
+    path_mock.exists(to_dir).AndReturn(True)
+
+    file_system_mock = mox_factory.CreateMock(os)
+    file_system_mock.symlink(from_file, to_file)
+
+    mox_factory.ReplayAll()
+
+    observed = create_symlinks_to_third_party_java_files.CreateSymlink(
+        from_file, to_file, path=path_mock, file_system=file_system_mock)
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+  def testCreateSymlinkWhenToDirDoesNotExist(self):
+    expected = True
+
+    from_file = 'from/file'
+    to_file = 'to/file'
+    to_dir = 'to'
+
+    mox_factory = mox.Mox()
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.exists(from_file).AndReturn(True)
+    path_mock.exists(to_file).AndReturn(False)
+    path_mock.dirname(to_file).AndReturn(to_dir)
+    path_mock.exists(to_dir).AndReturn(False)
+
+    file_system_mock = mox_factory.CreateMock(os)
+    file_system_mock.makedirs(to_dir)
+    file_system_mock.symlink(from_file, to_file)
+
+    mox_factory.ReplayAll()
+
+    observed = create_symlinks_to_third_party_java_files.CreateSymlink(
+        from_file, to_file, path=path_mock, file_system=file_system_mock)
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+
+class SyntaxParserTestCase(unittest.TestCase):
+  def testProcessRootPackageClassnameWhenFromFileExists(self):
+    expected = ('/root', 'package', 'Class.java')
+
+    mox_factory = mox.Mox()
+
+    mox_factory.StubOutWithMock(
+        create_symlinks_to_third_party_java_files, 'CreateSymlink')
+    create_symlinks_to_third_party_java_files.CreateSymlink(
+        '/root/package/Class.java', 'package/Class.java').AndReturn(expected)
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.join('package', 'Class.java').AndReturn('package/Class.java')
+    path_mock.join(
+        '/root', 'package/Class.java').AndReturn('/root/package/Class.java')
+
+    mox_factory.ReplayAll()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    observed = SyntaxParser._ProcessRootPackageClassname(
+        '/root', 'package', 'Class', path=path_mock)
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+  def testProcessRootPackageClassnameWhenFromFileDoesNotExist(self):
+    expected = None
+
+    mox_factory = mox.Mox()
+
+    mox_factory.StubOutWithMock(
+        create_symlinks_to_third_party_java_files, 'CreateSymlink')
+    create_symlinks_to_third_party_java_files.CreateSymlink(
+        '/root/package/Class.java', 'package/Class.java').AndReturn(expected)
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.join('package', 'Class.java').AndReturn('package/Class.java')
+    path_mock.join(
+        '/root', 'package/Class.java').AndReturn('/root/package/Class.java')
+
+    mox_factory.ReplayAll()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    observed = SyntaxParser._ProcessRootPackageClassname(
+        '/root', 'package', 'Class', path=path_mock)
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+
+class ImportParserTestCase(unittest.TestCase):
+  def testMatch(self):
+    import_parser = create_symlinks_to_third_party_java_files.ImportParser([])
+
+    observed = import_parser.Matches('import Class')
+
+    self.assertTrue(observed)
+
+  def testNoMatch(self):
+    import_parser = create_symlinks_to_third_party_java_files.ImportParser([])
+
+    observed = import_parser.Matches('// no match')
+
+    self.assertFalse(observed)
+
+  def testNewFilesWhenClassIsIn1stDirectoryInClasspath(self):
+    expected = [('/root-0', 'package', 'Class.java')]
+
+    import_line = 'import package.Class;'
+    classpath = ['/root-0', '/root-1']
+
+    import_parser = (
+        create_symlinks_to_third_party_java_files.ImportParser(classpath))
+    import_parser._line = 'import package.Class;'
+
+    mox_factory = mox.Mox()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    mox_factory.StubOutWithMock(SyntaxParser, '_ProcessRootPackageClassname')
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root-0', 'package', 'Class').AndReturn(expected[0])
+
+    mox_factory.ReplayAll()
+
+    observed = import_parser.NewFiles(None, None)
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+  def testNewFilesWhenClassIsInNthDirectoryInClasspath(self):
+    expected = [('/root-1', 'package', 'Class.java')]
+
+    import_line = 'import package.Class;'
+    classpath = ['/root-0', '/root-1']
+
+    import_parser = (
+        create_symlinks_to_third_party_java_files.ImportParser(classpath))
+    import_parser._line = 'import package.Class;'
+
+    mox_factory = mox.Mox()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    mox_factory.StubOutWithMock(SyntaxParser, '_ProcessRootPackageClassname')
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root-0', 'package', 'Class').AndReturn(None)
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root-1', 'package', 'Class').AndReturn(expected[0])
+
+    mox_factory.ReplayAll()
+
+    observed = import_parser.NewFiles(None, None)
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+
+class ExtendsOrNewParserTestCase(unittest.TestCase):
+  def testMatchForExtend(self):
+    extends_or_new_parser = (
+        create_symlinks_to_third_party_java_files.ExtendsOrNewParser())
+
+    observed = extends_or_new_parser.Matches('ClassA extends ClassB {')
+
+    self.assertTrue(observed)
+
+  def testMatchForNew(self):
+    extends_or_new_parser = (
+        create_symlinks_to_third_party_java_files.ExtendsOrNewParser())
+
+    observed = extends_or_new_parser.Matches('class_a = new ClassA();')
+
+    self.assertTrue(observed)
+
+  def testNoMatch(self):
+    extends_or_new_parser = (
+        create_symlinks_to_third_party_java_files.ExtendsOrNewParser())
+
+    observed = extends_or_new_parser.Matches('// no match')
+
+    self.assertFalse(observed)
+
+  def testNewFiles(self):
+    expected = [('/root', 'package', 'ClassB.java')]
+
+    line = 'ClassA extends ClassB {'
+
+    ExtendsOrNewParser = (
+        create_symlinks_to_third_party_java_files.ExtendsOrNewParser)
+    extends_or_new_parser = ExtendsOrNewParser()
+    extends_or_new_parser._matches = ExtendsOrNewParser.RE.search(line)
+
+    mox_factory = mox.Mox()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    mox_factory.StubOutWithMock(SyntaxParser, '_ProcessRootPackageClassname')
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root', 'package', 'ClassB').AndReturn(expected[0])
+
+    mox_factory.ReplayAll()
+
+    observed = extends_or_new_parser.NewFiles('/root', 'package')
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+
+class ThrowsParserTestCase(unittest.TestCase):
+  def testMatch(self):
+    throws_parser = create_symlinks_to_third_party_java_files.ThrowsParser()
+
+    observed = throws_parser.Matches('void Method() throws Class {')
+
+    self.assertTrue(observed)
+
+  def testNoMatch(self):
+    throws_parser = create_symlinks_to_third_party_java_files.ThrowsParser()
+
+    observed = throws_parser.Matches('// no match')
+
+    self.assertFalse(observed)
+
+  def testNewFiles(self):
+    expected = [('/root', 'package', 'ClassA.java'),
+                ('/root', 'pakcage', 'ClassB.java')]
+
+    line = 'void Method() throws ClassA, ClassB {'
+
+    throws_parser = create_symlinks_to_third_party_java_files.ThrowsParser()
+    throws_parser._matches = (
+        create_symlinks_to_third_party_java_files.ThrowsParser.RE.search(line))
+
+    mox_factory = mox.Mox()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    mox_factory.StubOutWithMock(SyntaxParser, '_ProcessRootPackageClassname')
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root', 'package', 'ClassA').AndReturn(expected[0])
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root', 'package', 'ClassB').AndReturn(expected[1])
+
+    mox_factory.ReplayAll()
+
+    observed = throws_parser.NewFiles('/root', 'package')
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+
+class StaticParserTestCase(unittest.TestCase):
+  def testMatch(self):
+    static_parser = create_symlinks_to_third_party_java_files.StaticParser()
+
+    observed = (
+        static_parser.Matches('ClassA class_a = ClassB.Method();'))
+
+    self.assertTrue(observed)
+
+  def testNoMatch(self):
+    static_parser = create_symlinks_to_third_party_java_files.StaticParser()
+
+    observed = static_parser.Matches('// no match')
+
+    self.assertFalse(observed)
+
+  def testNewFiles(self):
+    expected = [('/root', 'package', 'ClassB.java')]
+
+    line = 'ClassA class_a = ClassB.Method();'
+
+    static_parser = create_symlinks_to_third_party_java_files.StaticParser()
+    static_parser._matches = (
+        create_symlinks_to_third_party_java_files.StaticParser.RE.search(line))
+
+    mox_factory = mox.Mox()
+
+    SyntaxParser = create_symlinks_to_third_party_java_files.SyntaxParser
+    mox_factory.StubOutWithMock(SyntaxParser, '_ProcessRootPackageClassname')
+    SyntaxParser._ProcessRootPackageClassname(
+        '/root', 'package', 'ClassB').AndReturn(expected[0])
+
+    mox_factory.ReplayAll()
+
+    observed = static_parser.NewFiles('/root', 'package')
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+    mox_factory.UnsetStubs()
+
+
+class LineProcessorTestCase(unittest.TestCase):
+  def testFilterOutNone(self):
+    expected = ['not None 0', 'not None 1']
+
+    observed = (
+        create_symlinks_to_third_party_java_files.LineProcessor._FilterOutNone(
+            [None, 'not None 0', None, 'not None 1', None]))
+
+    self.assertEqual(expected, observed)
+
+  def testRegister(self):
+    class ParserDummy(object):
+      pass
+
+    parser_dummy = ParserDummy()
+
+    expected = [parser_dummy]
+
+    line_processor = create_symlinks_to_third_party_java_files.LineProcessor()
+    line_processor.Register(parser_dummy)
+
+    observed = line_processor._parsers
+
+    self.assertEquals(expected, observed)
+
+  def testProcessWithMatch(self):
+    expected = [('/root', 'package', 'Class.java')]
+
+    mox_factory = mox.Mox()
+
+    line = '// matches parser_mock_1'
+
+    parser_mock_0 = mox_factory.CreateMock(
+        create_symlinks_to_third_party_java_files.SyntaxParser)
+    parser_mock_0.Matches(line).AndReturn(False)
+    parser_mock_1 = mox_factory.CreateMock(
+        create_symlinks_to_third_party_java_files.SyntaxParser)
+    parser_mock_1.Matches(line).AndReturn(True)
+    parser_mock_1.NewFiles('/root', 'package').AndReturn(expected)
+    parser_mock_2 = mox_factory.CreateMock(
+        create_symlinks_to_third_party_java_files.SyntaxParser)
+
+    line_processor = create_symlinks_to_third_party_java_files.LineProcessor()
+    line_processor._parsers = [parser_mock_0, parser_mock_1, parser_mock_2]
+
+    mox_factory.ReplayAll()
+
+    observed = line_processor.Process('/root', 'package', line)
+
+    mox_factory.VerifyAll()
+    self.assertEquals(expected, observed)
+
+  def testProcessWithNoMatch(self):
+    expected = []
+
+    mox_factory = mox.Mox()
+
+    line = '// no match'
+
+    parser_mock_0 = mox_factory.CreateMock(
+        create_symlinks_to_third_party_java_files.SyntaxParser)
+    parser_mock_0.Matches(line).AndReturn(False)
+
+    line_processor = create_symlinks_to_third_party_java_files.LineProcessor()
+    line_processor._parsers = [parser_mock_0]
+
+    mox_factory.ReplayAll()
+
+    observed = line_processor.Process(None, None, line)
+
+    mox_factory.VerifyAll()
+    self.assertEquals(expected, observed)
+
+
+class FileProcessorTestCase(unittest.TestCase):
+  def testProcessLines(self):
+    class FileProcessorSpy(
+        create_symlinks_to_third_party_java_files.FileProcessor):
+      def __init__(self, line_processor):
+        super(FileProcessorSpy, self).__init__(line_processor)
+        self._process_files_args = []
+
+      def Process(self, files):
+        self._process_files_args.append(files)
+
+    expected = [[], [('/root', 'package', 'Class.java')]]
+
+    line0 = '// no match'
+    line1 = 'import package.Class;'
+    lines = [line0, line1]
+
+    mox_factory = mox.Mox()
+
+    line_processor = mox_factory.CreateMock(
+        create_symlinks_to_third_party_java_files.LineProcessor)
+    line_processor.Process('/root', 'package', line0).AndReturn([])
+    line_processor.Process('/root', 'package', line1).AndReturn(
+        [('/root', 'package', 'Class.java')])
+
+    file_processor_spy = FileProcessorSpy(line_processor)
+
+    mox_factory.ReplayAll()
+
+    file_processor_spy._ProcessLines('/root', 'package', lines)
+
+    observed = file_processor_spy._process_files_args
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+  def testProcess(self):
+    class FileProcessorSpy(
+        create_symlinks_to_third_party_java_files.FileProcessor):
+      def __init__(self, line_processor):
+        super(FileProcessorSpy, self).__init__(line_processor)
+        self._process_lines_args = []
+
+      def _ProcessLines(self, root, package, lines):
+        self._process_lines_args.append((root, package, lines))
+
+    class FileStub(object):
+      def xreadlines(self):
+        return ['// no match']
+
+    expected = [('/root-0', 'package', ['// no match']),
+                ('/root-1', 'package', ['// no match'])]
+
+    file0 = ('/root-0', 'package', 'ClassA.java')
+    file1 = ('/root-1', 'package', 'ClassB.java')
+    files = [file0, file1]
+
+    mox_factory = mox.Mox()
+
+    builtins_mock = mox_factory.CreateMock(__builtins__)
+    builtins_mock.file('/root-0/package/ClassA.java').AndReturn(FileStub())
+    builtins_mock.file('/root-1/package/ClassB.java').AndReturn(FileStub())
+
+    path_mock = mox_factory.CreateMock(os.path)
+    path_mock.join(
+        file0[0], file0[1], file0[2]).AndReturn('/root-0/package/ClassA.java')
+    path_mock.join(
+        file1[0], file1[1], file1[2]).AndReturn('/root-1/package/ClassB.java')
+
+    file_processor_spy = FileProcessorSpy(None)
+
+    mox_factory.ReplayAll()
+
+    file_processor_spy.Process(
+        files, file_creator=builtins_mock.file, path=path_mock)
+
+    observed = file_processor_spy._process_lines_args
+
+    mox_factory.VerifyAll()
+    self.assertEqual(expected, observed)
+
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ src/org/pwsafe/android/RecordEditPresenter.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2008 Noel Yap <noel.yap+code.google.com+com.pwsafe.android@gmail.com>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package org.pwsafe.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * RecordEditPresenter presents a form to edit Password Safe records.
+ *
+ * @author Noel Yap
+ */
+public class RecordEditPresenter {
+    private static final int DELETE_ID = Menu.FIRST;
+
+    private RecordEditView mView;
+
+    private EditText mRecordUrlText;
+    private EditText mRecordPassphraseText;
+    private EditText mRecordUsernameText;
+
+    public RecordEditPresenter(RecordEditView view) {
+        mView = view;
+    }
+
+    // TODO: Extract method.
+    /**
+     * Creates form or fields for record.
+     */
+    protected void onCreate(Bundle savedInstanceState) {
+        mView.setContentView(R.layout.record_edit);
+
+        mRecordPassphraseText = (EditText) mView.findViewById(R.id.record_passphrase);
+        mRecordUrlText = (EditText) mView.findViewById(R.id.record_url);
+        mRecordUsernameText = (EditText) mView.findViewById(R.id.record_username);
+
+        String recordPassphrase = getStringField(savedInstanceState, Util.RECORD_PASSPHRASE_FIELD);
+        if (recordPassphrase != null) {
+            mRecordPassphraseText.setText(recordPassphrase);
+        }
+
+        String recordUrl = getStringField(savedInstanceState, Util.RECORD_URL_FIELD);
+        if (recordUrl != null) {
+            mRecordUrlText.setText(recordUrl);
+        }
+
+        String recordUsername = getStringField(savedInstanceState, Util.RECORD_USERNAME_FIELD);
+        if (recordUsername != null) {
+            mRecordUsernameText.setText(recordUsername);
+        }
+
+        Button confirmButton = (Button) mView.findViewById(R.id.confirm);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent();
+
+                intent.putExtra(Util.RECORD_PASSPHRASE_FIELD, mRecordPassphraseText.getText().toString());
+                intent.putExtra(Util.RECORD_URL_FIELD, mRecordUrlText.getText().toString());
+                intent.putExtra(Util.RECORD_USERNAME_FIELD, mRecordUsernameText.getText().toString());
+
+                mView.setResult(Activity.RESULT_OK, intent);
+                mView.finish();
+            }
+        });
+    }
+
+    // TODO: Extract method so it can be reused.
+    /**
+     * Retrieves saved field value.
+     *
+     * @param savedInstanceState  holds stored fields
+     * @param fieldName  name of field to retrieve
+     * @return  value of field
+     */
+    private String getStringField(Bundle savedInstanceState, String fieldName) {
+        String result = (savedInstanceState != null
+                ? savedInstanceState.getString(fieldName)
+                : null);
+
+        if (result == null) {
+            Bundle extras = mView.getIntent().getExtras();
+
+            result = (extras != null
+                    ? extras.getString(fieldName)
+                    : null);
+        }
+
+        if (result == null) {
+            // TODO: Popup error dialog.
+        }
+
+        return result;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case DELETE_ID: {
+                deleteRecord();
+                return true;
+            }
+
+            default: {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Deletes record.
+     */
+    private void deleteRecord() {
+        Intent intent = new Intent();
+
+        intent.putExtra(Util.RECORD_PASSPHRASE_FIELD, (String) null);
+        intent.putExtra(Util.RECORD_URL_FIELD, (String) null);
+        intent.putExtra(Util.RECORD_USERNAME_FIELD, (String) null);
+
+        mView.setResult(Activity.RESULT_OK, intent);
+        mView.finish();
+    }
+}
