4c7c12dce259c3dd0be27954bf46b514a25af5ee
noel.yap <noel.yap@67170cb4-ae01-11dd-9253-9d056a18d4a0>
2009-02-01 01:44:44 +0000

    Unused imports cleaned up.

--- trunk/src/org/pwsafe/android/RecordListPresenter.java
+++ trunk/src/org/pwsafe/android/RecordListPresenter.java
@@ -30,8 +30,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
