a215ef3ce5f78a4fa18bf3cb6bbeb1d226568aca
noel.yap <noel.yap@67170cb4-ae01-11dd-9253-9d056a18d4a0>
2009-02-18 05:36:44 +0000

    Test created for Util.getDatabaseDir(Context).

--- test/org/pwsafe/android/UtilFunctionalTest.java
+++ test/org/pwsafe/android/UtilFunctionalTest.java
@@ -1,25 +1,19 @@
+/*
+ * Copyright (c) 2009 Noel Yap <noel.yap+code.google.com+com.pwsafe.android@gmail.com>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 package org.pwsafe.android;
 
-import edu.cornell.lassp.houle.RngPack.RandomElement;
-import edu.cornell.lassp.houle.RngPack.Ranmar;
-import edu.cornell.lassp.houle.RngPack.RanMT;
 import junit.framework.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class UtilFunctionalTest {
-    private static RandomElement mCharacterPrng;
-    private static RandomElement mLengthPrng;
-
-    @BeforeClass
-    public static void setUpTestCase() {
-        mCharacterPrng = new Ranmar();
-        mLengthPrng = new RanMT();
-    }
-
     @Test
     public void testDecodeEncode() {
-        String expected = createAnonymousText();
+        String expected = TestUtil.createAnonymousText();
 
         String observed = Util.decode(Util.encode(expected));
 
@@ -28,40 +22,10 @@ public class UtilFunctionalTest {
 
     @Test
     public void testEncodeDecode() {
-        String expected = createAnonymousHexString();
+        String expected = TestUtil.createAnonymousHexString();
 
         String observed = Util.encode(Util.decode(expected));
 
         Assert.assertEquals(expected, observed);
-    }
-
-    private String createAnonymousHexString() {
-        int length = 2*mLengthPrng.choose(256);
-
-        char[] result = new char[length];
-
-        for (int i = 0; i != length; ++i) {
-            int hex = mCharacterPrng.choose(16)-1;
-
-            if (hex < 10) {
-                result[i] = (char) ('0' + hex);
-            } else {
-                result[i] = (char) ('a' + (hex - 10));
-            }
-        }
-
-        return new String(result);
-    }
-
-    private String createAnonymousText() {
-        int length = mLengthPrng.choose(256);
-
-        char[] result = new char[length];
-
-        for (int i = 0; i != length; ++i) {
-            result[i] = (char) (mCharacterPrng.choose(256)-1);
-        }
-
-        return new String(result);
     }
 }
