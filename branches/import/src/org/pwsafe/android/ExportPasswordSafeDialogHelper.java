db390f632170bb0f4c3158b377541104db4bbefe
shawn.ledbetter <shawn.ledbetter@67170cb4-ae01-11dd-9253-9d056a18d4a0>
2009-05-28 00:35:00 +0000

    lazy export mechanism

---
+++ branches/import/src/org/pwsafe/android/ExportPasswordSafeDialogHelper.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2009 Noel Yap <noel.yap+code.google.com+com.pwsafe.android@gmail.com>.
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package org.pwsafe.android;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+
+public class ExportPasswordSafeDialogHelper extends DialogHelper {
+    public ExportPasswordSafeDialogHelper(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    public Dialog onCreateDialog() {
+        return new AlertDialog.Builder(GetActivity())
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setMessage(R.string.export_text)
+                .setTitle(R.string.exportdb)
+                .create();
+    }
+}
